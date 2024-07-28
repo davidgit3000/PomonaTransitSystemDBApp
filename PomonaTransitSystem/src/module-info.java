ef8c4522b96551bb7272ce5031d26720c3e7ed41
davidgit3000 <davidkarik5@gmail.com>
2024-07-27 20:45:42 -0700

    build the first window

--- PomonaTransitSystem/src/module-info.java
+++ PomonaTransitSystem/src/module-info.java
@@ -1,5 +1,8 @@
 module PomonaTransitSystem {
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.logging;
 	
 	opens pts to javafx.graphics, javafx.fxml;
 }
--- PomonaTransitSystem/src/pts/Main.java
+++ PomonaTransitSystem/src/pts/Main.java
@@ -1,22 +1,27 @@
 package pts;
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+			Scene scene = new Scene(root);
+			scene.setFill(Color.TRANSPARENT);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch(IOException ex) {
+			 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 	
